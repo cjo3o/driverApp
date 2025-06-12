452fa9e57a137afbb6a9d448ac83f730a9bea76e
DESKTOP-29VI7GO\user <kimyoott@gmail.com>
2025-06-12 18:14:28 +0900

    no message

--- app.js
+++ app.js
@@ -6,13 +6,20 @@ const app = express();
 app.set('port', 7777);
 app.set('view engine', 'html');
 
+app.use("/public",express.static('public'));
+
 nunjucks.configure("views",{
     express:app,
     watch:true,
 })
 
 const mainRouter = require('./routes/mainRouter.js');
+const bellRouter = require('./routes/bellRouter.js');
+const moneyRouter = require('./routes/moneyRouter.js');
+
 app.use('/', mainRouter);
+app.use('/bell', bellRouter);
+app.use('/money', moneyRouter);
 
 app.listen(app.get('port'), ()=>{
     console.log(app.get('port'), '번 포트에서 대기 중')
