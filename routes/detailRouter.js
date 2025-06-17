216a5ebc53442e8b229c5c72be59819af3e9d499
DESKTOP-29VI7GO\user2 <cjy55973743@gmail.com>
2025-06-17 18:12:10 +0900

    no message

--- routes/detailRouter.js
+++ routes/detailRouter.js
@@ -1,8 +1,47 @@
 const express = require('express');
 const router = express.Router();
+const supabase = require('../utils/supa');
 
-router.get('/', (req, res) => {
-    res.render('detail');
+router.get('/', async (req, res) => {
+    console.log('req.query:', req.query);
+    const { re_num } = req.query;
+
+    let deliveryData = null;
+
+    if (re_num) {
+        try {
+            const { data, error } = await supabase
+                .from('delivery')
+                .select('*')
+                .eq('re_num', re_num)
+                .single();
+
+            if (error) {
+                console.error('Supabase 오류:', error);
+            } else {
+                deliveryData = data;
+                console.log('배송 데이터:', deliveryData);
+            }
+        } catch (error) {
+            console.error('서버 오류:', error);
+        }
+    }
+
+    res.render('detail', {
+        title: '상세보기',
+        deliveryData
+    });
+});
+
+router.post('/', async (req, res) => {
+    const {re_num} = req.body;
+    const {data, error} = await supabase
+        .from('delivery')
+        .update({
+            situation: '배송대기'
+        })
+        .eq('re_num', re_num);
+    console.log('data', data);
 });
 
 module.exports = router;
--- routes/mainRouter.js
+++ routes/mainRouter.js
@@ -1,17 +1,35 @@
 const express = require('express');
 const router = express.Router();
+const supabase = require('../utils/supa');
 
-router.get('',(req, res, next) => {
+router.get('/', async (req, res, next) => {
+    console.log('사진', req.session.user);
     const now = new Date();
     const days = ['일', '월', '화', '수', '목', '금', '토'];
     const dayOfWeek = days[now.getDay()];
     const month = now.getMonth() + 1;
     const date = now.getDate();
-    console.log(month);
-    console.log(date);
-    console.log(dayOfWeek);
-    res.render('main', {title: '메인페이지', user: req.session.user, month, date, dayOfWeek});
-})
+    let resData = [];
+    try {
+        const { data, error } = await supabase
+            .from('delivery')
+            .select('*')
+            .eq('situation', '접수');
+        resData = data;
+        console.log(resData);
+    } catch (error) {
+        console.log(error);
+    };
+
+    res.render('main', {
+        title: '메인페이지',
+        user: req.session.user,
+        month,
+        date,
+        dayOfWeek,
+        resData
+    });
+});
 
 
 module.exports = router;
