663e76a7e740de8534aa597b1633e3fdabc80d05
DESKTOP-29VI7GO\user2 <cjy55973743@gmail.com>
2025-06-26 15:58:36 +0900

    no message

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 app.use(expressSession({
     secret: 'asdf1234',
